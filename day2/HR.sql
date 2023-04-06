054be71167acc516c00120b94214d97eec96a22e
dnlpys <dnlpys9999@gmail.com>
2023-04-06 13:49:45 +0900

    230406 13:49

--- day2/HR.sql
+++ day2/HR.sql
@@ -96,9 +96,81 @@ select 1+(2*3) from dual;
 
 select employee_id,salary from employees
 where salary>=20000 or salary <= 2100
---union --중복된거은 한개만
+union --중복된거은 한개만
 --union all --모두다
 --intersect --중복된것이 뭐냐?
-minus --먼저검색한 결과에서 뒤에검색결과를 빼기.
+--minus --먼저검색한 결과에서 뒤에검색결과를 빼기.
 select employee_id,salary from employees
 where salary <= 2200;
+
+--in : 여러 이퀄 or 연산
+SELECT DEPARTMENT_ID, SALARY FROM employees
+WHERE DEPARTMENT_ID IN(50, 20, 100);
+
+SELECT distinct DEPARTMENT_ID FROM employees
+WHERE DEPARTMENT_ID IN(50, 20, 100);
+
+--여러 크다,작다,이상,이하
+--any : >일때 데이터들중에서 가장작은것보다 큰것들
+--any : >=일때 데이터들중에서 가장작은것 이상인것들
+--any : <일때 데이터들중에서 가장큰것보다 작은것들
+SELECT DEPARTMENT_ID, SALARY FROM employees
+WHERE DEPARTMENT_ID >any(50, 20, 100); -- 작큰
+SELECT DEPARTMENT_ID, SALARY FROM employees
+WHERE DEPARTMENT_ID <any(50, 20, 100); -- 큰작
+
+SELECT DEPARTMENT_ID, SALARY FROM employees
+WHERE DEPARTMENT_ID >all(50, 20, 100); --큰큰
+SELECT DEPARTMENT_ID, SALARY FROM employees
+WHERE DEPARTMENT_ID <all(50, 20, 100); --작작
+
+
+SELECT employee_id, SALARY FROM employees
+WHERE salary in(4400,17000,10000,12000);
+
+SELECT employee_id, SALARY FROM employees
+WHERE salary <=any(4400,17000,10000,12000);
+
+select first_name,last_name from employees
+where last_name like '%t%';
+select first_name,last_name from employees
+where last_name like '%in%';
+
+select first_name,last_name from employees
+where last_name like '_e%';
+
+-- abctde a,t
+select first_name,last_name from employees
+where last_name like '%a%t%';
+
+select first_name,last_name from employees
+where last_name like '%a%' and last_name like '%t%';
+
+select first_name,last_name from employees;
+select first_name||last_name from employees;
+select 
+first_name||' '||last_name||'의 연봉['||salary||']' info
+from employees;
+
+SELECT EMPLOYEE_ID FROM employees
+WHERE NOT (SALARY > 1000 AND SALARY < 12000);
+
+SELECT EMPLOYEE_ID FROM employees
+WHERE NOT SALARY > 1000 AND SALARY < 12000;
+
+drop table test2;
+create table test2(fname varchar2(20));
+insert into test2 values('S1234');
+insert into test2 values('1234S_1234');
+insert into test2 values('$S_1234');
+insert into test2 values('sssS_1234');
+insert into test2 values('THE X\_Y');
+insert into test2 values('THE1 X_Y');
+insert into test2 values('THE X\&Y');
+insert into test2 values('THE X&Y');
+insert into test2 values('THE2 X_Y');
+insert into test2 values('THE2 90%');
+insert into test2 values('THE2 30%');
+
+select fname from test2 where fname like '%\%%' escape '\';
+select fname from test2 where fname like '%\_%' escape '\' ;
