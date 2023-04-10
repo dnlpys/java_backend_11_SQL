d5691ec38c1f7f7cd6b4a51c107ccdc8f3738e8e
dnlpys <dnlpys9999@gmail.com>
2023-04-10 16:02:07 +0900

    230410 16:02

--- pro02jdbc/src/test/com/TestDAOimpl.java
+++ pro02jdbc/src/test/com/TestDAOimpl.java
@@ -1,25 +1,124 @@
 package test.com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class TestDAOimpl implements TestDAO{
+public class TestDAOimpl implements TestDAO {
+
+	private Connection conn; // 커넥션용 객체
+	private PreparedStatement pstmt; // 질의문 요청용 객체
+	private ResultSet rs; // select질의문 응답객체
 	
+	private String driver_name = "oracle.jdbc.OracleDriver";
+	private final String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private final String user = "hr";
+	private final String password = "hi123456";
+
 	public TestDAOimpl() {
 		System.out.println("TestDAOimpl()...");
+		try {
+			Class.forName(driver_name);
+			System.out.println("Driver successed...");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public List<TestVO> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("selectAll()...");
+
+		try {
+			conn = DriverManager.getConnection(
+					url, 
+					user, 
+					password);
+			System.out.println("conn successed...");
+
+			pstmt = conn.prepareStatement("select * from test");
+
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				System.out.print(rs.getInt("num"));
+				System.out.print(rs.getString("name"));
+				System.out.println(rs.getInt("age"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		List<TestVO> vos = new ArrayList<>();
+		TestVO vo = new TestVO();
+		vo.setNum(1);
+		vo.setName("kim1");
+		vo.setAge(11);
+		vos.add(vo);
+
+		vo = new TestVO();
+		vo.setNum(2);
+		vo.setName("kim1");
+		vo.setAge(11);
+		vos.add(vo);
+
+		vo = new TestVO();
+		vo.setNum(3);
+		vo.setName("kim3");
+		vo.setAge(11);
+		vos.add(vo);
+
+		return vos;
 	}
 
 	@Override
 	public TestVO selectOne(TestVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		System.out.println("selectOne()...");
+		System.out.println(vo);
 
-	
+		try {
+			conn = DriverManager.getConnection(
+					url, 
+					user, 
+					password);
+			System.out.println("conn successed...");
+
+			pstmt = conn.prepareStatement("select * from test where num=3");
+
+			rs = pstmt.executeQuery();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		TestVO vo2 = new TestVO();
+		vo2.setNum(3);
+		vo2.setName("kim3");
+		vo2.setAge(11);
+		System.out.println(vo2);
+
+		return vo2;
+	}
 
 }
--- pro02jdbc/src/test/com/TestMain.java
+++ pro02jdbc/src/test/com/TestMain.java
@@ -1,5 +1,7 @@
 package test.com;
 
+import java.util.List;
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -9,10 +11,25 @@ public class TestMain {
 		//***java코드 + DB
 		//1.사용할 data객체(VO,DTO) 선정 >> 2.DB테이블의 행데이터를 결정
 		TestDAO dao = new TestDAOimpl();
-		//data 처리객체(3.인터페이스(DAO),4.클래스 상속관계(DAOimpl))
-		//상속받은 클래스를 통해 DB와 연동하는 로직 작성
-		//	연동순서(5.드라이버연결,6.커넥션,7.질의문요청및 응답,8.예외처리)
+		List<TestVO> vos = dao.selectAll();
+		System.out.println(vos);
 		
+		TestVO vo = new TestVO();
+		vo.setNum(3);
+		
+		TestVO vo2 = dao.selectOne(vo);
+		System.out.println(vo2);
+		
+		//data 처리객체(3.인터페이스(DAO),4.클래스 상속관계(DAOimpl))
+		//4-1.seleceAll
+		//4-2.selectOne
+		//상속받은 클래스를 통해 DB와 연동하는 로직 작성
+		//	연동순서(5.드라이버연결,6.커넥션,7.질의문 요청및 응답,8.예외처리)
+		
+//		C:\oraclexe\app\oracle\product\11.2.0\server\jdbc\lib
+		//ojdbc6.jar 파일복사
+		//프로젝트 우클리>new>folder>lib : ojdcb6.jar파일 붙여넣기
+		//ojdcb6.jar파일 우클릭 Build path >> Add to build path
 		
 	}//end main
 
