40c335cf33271b45802bb954692fe7575de11ba2
dnlpys <dnlpys9999@gmail.com>
2023-04-13 15:42:15 +0900

    230413 15:42

--- day7/pro06emp/src/test/com/EmpDAOimpl.java
+++ day7/pro06emp/src/test/com/EmpDAOimpl.java
@@ -37,8 +37,7 @@ public class EmpDAOimpl implements EmpDAO {
 			conn = DriverManager.getConnection(url, user, password);
 			System.out.println("conn successed...");
 
-			pstmt = conn.prepareStatement(
-					"select * from emp order by employee_id desc");
+			pstmt = conn.prepareStatement("select * from emp order by employee_id desc");
 
 			rs = pstmt.executeQuery();
 
@@ -84,32 +83,261 @@ public class EmpDAOimpl implements EmpDAO {
 
 	@Override
 	public EmpVO selectOne(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("selectOne()..."+vo);
+
+		EmpVO vo2 = new EmpVO();
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("conn successed...");
+
+			pstmt = conn.prepareStatement("select * from emp where employee_id = ?");
+			pstmt.setInt(1, vo.getEmployee_id());
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				vo2.setEmployee_id(rs.getInt("employee_id"));
+				vo2.setFirst_name(rs.getString("first_name"));
+				vo2.setLast_name(rs.getString("last_name"));
+				vo2.setSalary(rs.getInt("salary"));
+				vo2.setHire_date(rs.getDate("hire_date"));
+				vo2.setJob_id(rs.getString("job_id"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} // end finally
+
+		return vo2;
 	}
 
 	@Override
 	public List<EmpVO> searchList(String searchKey, String searchWord) {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("searchList()...");
+		System.out.println("searchKey..." + searchKey);
+		System.out.println("searchWord..." + searchWord);
+		List<EmpVO> vos = new ArrayList<>();
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("conn successed...");
+
+			String sql = "";
+			if (searchKey.equals("job_id")) {
+				sql = "select * from emp where job_id like ? order by employee_id desc";
+			} else {
+				sql = "select * from emp where first_name like ? order by employee_id desc";
+			}
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "%" + searchWord + "%");
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				EmpVO vo = new EmpVO();
+				vo.setEmployee_id(rs.getInt("employee_id"));
+				vo.setFirst_name(rs.getString("first_name"));
+				vo.setLast_name(rs.getString("last_name"));
+				vo.setSalary(rs.getInt("salary"));
+				vo.setHire_date(rs.getDate("hire_date"));
+				vo.setJob_id(rs.getString("job_id"));
+				vos.add(vo);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} // end finally
+
+		return vos;
 	}
 
 	@Override
 	public int insert(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("insert()...."+vo);
+		int flag = 0;
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("conn successed...");
+
+			pstmt = conn.prepareStatement(
+					"insert into emp(employee_id,first_name,"
+					+ "last_name,salary,hire_date,job_id) "
+					+ "values(seq_emp_id.nextval,?,?,?,?,?)");
+			pstmt.setString(1, vo.getFirst_name());
+			pstmt.setString(2, vo.getLast_name());
+			pstmt.setInt(3, vo.getSalary());
+			pstmt.setDate(4, vo.getHire_date());
+			pstmt.setString(5, vo.getJob_id());
+			
+			flag = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} // end finally
+		
+		return flag;
 	}
 
 	@Override
 	public int update(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("update()...."+vo);
+		int flag = 0;
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("conn successed...");
+
+			pstmt = conn.prepareStatement(
+					"update emp set first_name=?, last_name=?,"
+					+ "salary=?,hire_date=?,job_id=? where employee_id=?");
+			pstmt.setString(1, vo.getFirst_name());
+			pstmt.setString(2, vo.getLast_name());
+			pstmt.setInt(3, vo.getSalary());
+			pstmt.setDate(4, vo.getHire_date());
+			pstmt.setString(5, vo.getJob_id());
+			pstmt.setInt(6, vo.getEmployee_id());
+			
+			flag = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} // end finally
+		
+		return flag;
 	}
 
 	@Override
 	public int delete(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("delete()...."+vo);
+		int flag = 0;
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("conn successed...");
+
+			pstmt = conn.prepareStatement(
+					"delete from emp where employee_id=?");
+			pstmt.setInt(1, vo.getEmployee_id());
+			
+			flag = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		} // end finally
+		
+		return flag;
 	}
 
 }
--- day7/pro06emp/src/test/com/EmpMain.java
+++ day7/pro06emp/src/test/com/EmpMain.java
@@ -9,21 +9,68 @@ public class EmpMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Emp");
-		
-		//employees테이블을 데이터포함 복제해서 emp 테이블을 만들고
-		//EmpVO에 있는 속성과 관련된 컬럼만 복제할것.
-		//입력,수정,삭제,모두검색,검색어검색,번호검색
-		
-		GregorianCalendar gcal = new GregorianCalendar(2020, 11, 25);
+
+		// employees테이블을 데이터포함 복제해서 emp 테이블을 만들고
+		// EmpVO에 있는 속성과 관련된 컬럼만 복제할것.
+		// 입력,수정,삭제,모두검색,검색어검색,번호검색
+
+//		GregorianCalendar gcal = new GregorianCalendar(2020, 11, 25);
 //		System.out.println(gcal.get(Calendar.YEAR));
 //		System.out.println(gcal.get(Calendar.MONTH));
 //		System.out.println(gcal.get(Calendar.DAY_OF_MONTH));
-		java.sql.Date hire_date = new Date(gcal.getTimeInMillis());
-		System.out.println(hire_date);
-		EmpVO vo = new EmpVO();
-		vo.setHire_date(hire_date);
-		
+//		java.sql.Date hire_date = new Date(gcal.getTimeInMillis());
+//		System.out.println(hire_date);
+//		EmpVO vo = new EmpVO();
+//		vo.setHire_date(hire_date);
+
 		EmpDAO dao = new EmpDAOimpl();
+
+		EmpVO vo = new EmpVO();
+		vo.setEmployee_id(100);
+		EmpVO vo2 = dao.selectOne(vo);
+		System.out.print(vo2.getEmployee_id() + "\t");
+		System.out.print(vo2.getFirst_name() + "\t");
+		System.out.print(vo2.getLast_name() + "\t");
+		System.out.print(vo2.getSalary() + "\t");
+		System.out.print(vo2.getHire_date() + "\t");
+		System.out.println(vo2.getJob_id());
+
+		System.out.println("==========================");
+//		vo = new EmpVO();
+//		vo.setFirst_name("Gildong");
+//		vo.setLast_name("Hong");
+//		vo.setSalary(5000);
+//		GregorianCalendar gcal = new GregorianCalendar(2023, 3, 12);
+//		Date hire_date = new Date(gcal.getTimeInMillis());
+//		vo.setHire_date(hire_date);
+//		vo.setJob_id("AC_MGR");
+//		int result = dao.insert(vo);
+//		System.out.println("result:"+result);
+
+//		System.out.println("==========================");
+//
+//		vo = new EmpVO();
+//		vo.setEmployee_id(208);
+//		vo.setFirst_name("Gildong3");
+//		vo.setLast_name("Hong3");
+//		vo.setSalary(15000);
+//		GregorianCalendar gcal = new GregorianCalendar(2022, 3, 12);
+//		Date hire_date = new Date(gcal.getTimeInMillis());
+//		vo.setHire_date(hire_date);
+//		vo.setJob_id("AC_ACCOUNT");
+//		int result = dao.update(vo);
+//		System.out.println("result:" + result);
+		
+		
+		System.out.println("==========================");
+
+		vo = new EmpVO();
+		vo.setEmployee_id(208);
+		int result = dao.delete(vo);
+		System.out.println("result:" + result);
+		
+		System.out.println("==========================");
+		
 		List<EmpVO> vos = dao.selectAll();
 		for (EmpVO x : vos) {
 			System.out.print(x.getEmployee_id() + "\t");
@@ -33,9 +80,10 @@ public class EmpMain {
 			System.out.print(x.getHire_date() + "\t");
 			System.out.println(x.getJob_id());
 		}
-		
-//		vos = dao.searchList("fname", "St");
-		vos = dao.searchList("job_id", "AC");
+		System.out.println("==========================");
+
+		vos = dao.searchList("fname", "St");
+//		vos = dao.searchList("job_id", "AC");
 		for (EmpVO x : vos) {
 			System.out.print(x.getEmployee_id() + "\t");
 			System.out.print(x.getFirst_name() + "\t");
@@ -44,7 +92,7 @@ public class EmpMain {
 			System.out.print(x.getHire_date() + "\t");
 			System.out.println(x.getJob_id());
 		}
-		
-	}//end main
 
-}//end class
+	}// end main
+
+}// end class
